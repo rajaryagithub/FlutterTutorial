b566073fe21aceb36b125d18803551b853de836c
Rajesh Kumar <Rajesh.Kumar@tomtom.com>
2020-01-03 11:49:04 +0100

    Example of modularization of code in classes

---
+++ hello_world/lib/app.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+import 'hello.dart';
+
+class App extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(home: Hello());
+  }
+}
---
+++ hello_world/lib/hello.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class Hello extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Title')),
+      body: Text('Body text'),
+    );
+  }
+}
